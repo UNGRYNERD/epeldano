4e5085aa249f41c55f5cef41cee793f65d423fdf
Daniel Aguilar <dacmail@gmail.com>
2018-05-10 10:57:24 +0200

    Noticia con logo de fondo

--- wp-content/themes/epeldano/index.php
+++ wp-content/themes/epeldano/index.php
@@ -23,11 +23,18 @@
         </div>
       </article>
     <?php else : ?>
-      <article <?php post_class('post-block ' . (get_field('black') ? 'post-block--black' : '')) ?> style="background-image: url(<?php the_post_thumbnail_url('ungrynerd_big'); ?>); background-color: <?php the_field('background') ?>">
+      <?php
+        $style = get_field('is_logo') ? '' : 'background-image: url(' . get_the_post_thumbnail_url(get_the_ID(), 'ungrynerd_big') . ');';
+        $style .= get_field('background') ? 'background-color:' . get_field('background') : '';
+      ?>
+      <article <?php post_class('post-block ' . (get_field('black') ? 'post-block--black' : '') . (get_field('is_logo') ? 'post-block--logo' : '')) ?> style="<?= esc_attr($style); ?>">
         <div class="post-block__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
         <?php the_excerpt(); ?>
         <p class="post-block__date"><?php the_time(get_option('date_format')); ?></p>
         <a href="<?php the_permalink(); ?>" class="post-block__more">+</a>
+        <?php if (get_field('is_logo')) : ?>
+          <?php the_post_thumbnail('ungrynerd_small', ['class' => 'post-block__logo']); ?>
+        <?php endif; ?>
       </article>
     <?php endif; ?>
   <?php endwhile; ?>
